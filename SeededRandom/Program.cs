4c17365bffbad95d0f0c20b28da603acdfa85c64
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2026-05-31 20:16:32 +0900

    不要なファイルを削除

--- SeededRandom/Program.cs
+++ SeededRandom/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-//var pool = new SeededRandomPool(1, 100, seed: 12345);
 
 var bag = new Mustard.ShuffleBag(Mustard.Seed.Restore("Hello"));
 
